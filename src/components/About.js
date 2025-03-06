2d829899012fbce1df94a4b402e0ab2012d8bf17
alphabit-dep <126347852+alphabit-dep@users.noreply.github.com>
2025-03-06 12:30:51 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -45,8 +45,8 @@ export default function About() {
               </p>
               <p className="mt-6 text-lg leading-8" data-aos="fade-right">
                 Programmer web yang percaya diri, optimis, dan disiplin dengan semangat kerja tinggi. Berpengalaman dalam mengembangkan aplikasi berbasis web yang efisien dan user-friendly menggunakan PHP (CodeIgniter 3), Laravel, dan JavaScript. 
-                Beberapa proyek yang telah saya selesaikan mencakup Aplikasi Web Monitoring BTS TELKOMSEL, Monitoring Layanan Indihome.<br>
-                Saat ini saya sebagai karyawan TELKOM AKSES di unit Digital Network Services, saya telah mengasah keterampilan dalam pengajaran dan pengembangan aplikasi. 
+                Beberapa proyek yang telah saya selesaikan mencakup Aplikasi Web Monitoring BTS TELKOMSEL, Monitoring Layanan Indihome.
+                Saat ini saya sebagai karyawan di TELKOM AKSES di unit Digital Network Services, saya telah mengasah keterampilan dalam pengajaran dan pengembangan aplikasi. 
                 Dengan minat besar dalam eksplorasi teknologi dan inovasi, saya berkomitmen untuk terus belajar dan memberikan hasil terbaik.
               </p>
             </div>
